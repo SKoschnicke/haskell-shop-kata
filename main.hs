d130af981be019723d61ee4a3fdd9cff85c92451
Sven Koschnicke <s.koschnicke@gfxpro.com>
2011-08-20 22:40:56 +0200

    added money

--- main.hs
+++ main.hs
@@ -1,2 +1,10 @@
 
+data Currency = Euro
+              | US_Dollar
+              | Yen
+              | Bitcoin
+
+-- money is an amount and a currency
+data Money = Rational Currency
+
 main = putStrLn "Hello world"
