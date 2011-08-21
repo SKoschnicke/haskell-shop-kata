4a487928c5e883b0b6e44f6b6c6136c1cbc71afb
Sven Koschnicke <s.koschnicke@gfxpro.com>
2011-08-21 09:43:16 +0200

    finished money, began with product definition

--- main.hs
+++ main.hs
@@ -1,10 +1,27 @@
-
 data Currency = Euro
               | US_Dollar
               | Yen
               | Bitcoin
+              deriving (Show,Eq)
 
 -- money is an amount and a currency
-data Money = Rational Currency
+-- TODO: is Double the right datatype for arbitrary precision decimals?
+data Money = Money {
+      amount   :: Double
+    , currency :: Currency
+    } deriving (Show,Eq)
+
+-- a product should not have a price or an amount
+data Product = Product {
+      name :: String
+    } deriving (Show)
+
+-- price definition is a product, a money instance and an matcher or something for the amount
+-- amount is a number and a unit
+
+-- TODO: Non-exhaustive patterns to ensure equality of currencies?
+addMoney :: Money -> Money -> Money
+addMoney (Money a currency_a) (Money b currency_b)
+  | currency_a == currency_b  = Money (a+b) currency_a
 
 main = putStrLn "Hello world"
